2d69ca508c63de22b36194a3f4777f917b85bce8
James Menzies <james@symphona.app>
2021-10-18 09:12:18 +1100

    env vars working

--- discord_interaction/lambda_handler.py
+++ discord_interaction/lambda_handler.py
@@ -1,10 +1,12 @@
 import json
+import os
 
+import boto3
 
 def handler(event, context):
+
+    print(os.environ['DISCORD_PUBLIC_KEY'])
+
     return {
         "statusCode": 200,
-        "body": {
-            json.dumps("Hello World")
-        }
     }
